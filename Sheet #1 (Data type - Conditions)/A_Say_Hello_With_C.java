3a9e746ea650fc4d5a5d4b27c7dd6baedaf9cba4
avaj-12 <99613855+avaj-12@users.noreply.github.com>
2023-04-09 16:00:23 +0530

    Create A_Say_Hello_With_C.java

---
+++ Sheet #1 (Data type - Conditions)/A_Say_Hello_With_C.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+// The main method must be in a class named "Main"
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        System.out.println("Hello," + " "+ s);
+    }
+}
