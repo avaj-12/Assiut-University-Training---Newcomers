6cb2e022125af2455cf3a952308307c30ba896de
avaj-12 <99613855+avaj-12@users.noreply.github.com>
2023-04-09 16:01:18 +0530

    Create B_Basic_Data_Types.java

---
+++ Sheet #1 (Data type - Conditions)/B_Basic_Data_Types.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main"
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        long  num1 = scan.nextLong();
+        char c = scan.next().charAt(0);
+        float f = scan.nextFloat();
+        double d = scan.nextDouble();
+
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+
+
+
+    }
+}
